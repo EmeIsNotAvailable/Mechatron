e48cb6bf8cf537ad56830d25051ba929862fdfa4
EmeIsNotAvailable <75790968+EmeIsNotAvailable@users.noreply.github.com>
2021-10-27 21:07:02 +0200

    soldaditos

---
+++ Mechatron/Mechatron/Assets/Scripts/Enemigo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    // Start is called before the first frame update
+  
+    public GameObject enemigos;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Fondo")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Hemos chocao");
+            enemigos.SendMessage("MovimientoX");
+            
+        }
+    }
+}
---
+++ Mechatron/Mechatron/Assets/Scripts/MovEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovEnemy : MonoBehaviour
+{
+    public float vel = 2.5f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        transform.Translate(Vector2.up * Time.deltaTime * vel);
+    }
+    public void MovimientoX()
+    {
+        transform.Translate(-1, 0, 0);
+        vel = -vel;
+    }
+}
